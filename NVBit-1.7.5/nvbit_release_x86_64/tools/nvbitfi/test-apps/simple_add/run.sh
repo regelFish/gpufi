59f5685507779f5939dfa4bb4a53f4c98892f67a
regelFish <168052544+regelFish@users.noreply.github.com>
2025-10-31 15:05:38 -0400

    stderr filter implemented

--- NVBit-1.7.5/nvbit_release_x86_64/tools/nvbitfi/test-apps/simple_add/run.sh
+++ NVBit-1.7.5/nvbit_release_x86_64/tools/nvbitfi/test-apps/simple_add/run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-eval ${PRELOAD_FLAG} ${BIN_DIR}/simple_add > stdout.txt 2> stderr.txt
+eval ${PRELOAD_FLAG} ${BIN_DIR}/simple_add > stdout.txt 2> >(grep -v "WARNING: Do not call CUDA memory allocation" > stderr.txt)
+
