e573a3134b016cca26a469faf1ef14c18c8693be
regelFish <168052544+regelFish@users.noreply.github.com>
2026-03-11 10:12:00 -0400

    Sadge

--- NVBit-1.7.5/nvbit_release_x86_64/tools/nvbitfi/test-apps/simple_add/run.sh
+++ NVBit-1.7.5/nvbit_release_x86_64/tools/nvbitfi/test-apps/simple_add/run.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-eval ${PRELOAD_FLAG} ncu --target-processes all --set full \
-  --export ncu_report --force-overwrite -- \
-  ${BIN_DIR}/simple_add \
-  > stdout.txt 2> >(grep -v "WARNING: Do not call CUDA memory allocation in nvbit_at_ctx_init(). It will cause deadlocks. Do them in nvbit_tool_init(). If you encounter deadlocks, remove CUDA API calls to debug." > stderr.txt)
+eval ${PRELOAD_FLAG} ${BIN_DIR}/simple_add > stdout.txt 2> >(grep -v "WARNING: Do not call CUDA memory allocation in nvbit_at_ctx_init(). It will cause deadlocks. Do them in nvbit_tool_init(). If you encounter deadlocks, remove CUDA API calls to debug." > stderr.txt)
+
